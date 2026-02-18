197144363c72a2bfd4bd8d9920636a1ff763a85a
A.S.M. Bin Rahman Ziko <mdziko44@gmail.com>
2026-02-18 17:53:14 +0600

    footer

--- resources/views/layout/inc/footer.blade.php
+++ resources/views/layout/inc/footer.blade.php
@@ -1,37 +1,90 @@
- <footer class="footer-area footer-default">
-    <div class="footer-wrapper">
-      <!-- Start Footer Style Two  -->
-        <div class="footer-style-2 ptb--30 bg_image bg_image--1" data-black-overlay="6">
-            <div class="wrapper plr--50 plr_sm--20">
-                <div class="row align-items-center justify-content-between">
-                    <div class="col-lg-4 col-md-6 col-sm-6 col-12">
-                        <div class="inner">
-                            <div class="logo text-center text-sm-start mb_sm--20">
-                                <a href="quazi.global">
-                                    <img src="{{ asset('assets/images/logo/logo-symbol-dark.png') }}" alt="Logo images" />
-                                </a>
-                            </div>
-                        </div>
+<!-- Start Footer -->
+<footer class="quazi-footer">
+    <div class="container" style="z-index:1;position:relative">
+        <div class="footer-top row">
+            <div class="col-md-8">
+                <div class="footer-content">
+                    <h2 class="footer-title">Why Quazi M. Ahmed is a Leader <br class="hidden-xs">to Leaders</h2>
+                    <div class="footer-text">
+                        Quazi M. Ahmed is a globally recognized leadership trainer and executive coach with over 27 years of experience transforming leaders across 13 countries. As an IFC-Certified Master Trainer, TEDx Speaker, and founder of multiple organizations, he has positively impacted more than 100,000 professionals and entrepreneurs worldwide, working with Fortune 500 companies, government bodies, and international organizations.
                     </div>
-                    <div class="col-lg-4 col-md-6 col-sm-6 col-12">
-                        <div class="inner text-center">
-                            <ul class="social-share rn-lg-size d-flex justify-content-center liststyle">
-                        <li><a href="https://www.facebook.com/quazi.ahmed"><i class="fab fa-facebook-f"></i></a></li>
-                        <li><a href="https://www.linkedin.com/in/quaziahmed/"><i class="fab fa-linkedin-in"></i></a></li>
-                            </ul>
-                        </div>
-                    </div>
-                    <div class="col-lg-4 col-md-12 col-sm-12 col-12">
-                        <div class="inner text-lg-end text-center mt_md--20 mt_sm--20">
-                            <div class="text">
-                                <p>Copyright © {{ date('Y') }} QUAZI.GLOBAL. All Rights Reserved.</p>
-
-                            </div>
+                    <div class="footer-featured">
+                        <span class="featured-label">Featured In:</span>
+                        <div class="featured-logos">
+                            <span class="featured-item">Forbes</span>
+                            <span class="featured-item">BBC</span>
+                            <span class="featured-item">CNN</span>
+                            <span class="featured-item">TEDx</span>
+                            <span class="featured-item">Entrepreneur</span>
                         </div>
                     </div>
                 </div>
             </div>
+            <div class="col-md-4">
+                <h3 class="footer-social-title">FOLLOW QUAZI</h3>
+                <div class="footer-social">
+                    <!-- LinkedIn -->
+                    <a href="https://www.linkedin.com/in/quaziahmed/" target="_blank" title="LinkedIn">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 24 24" fill="currentColor">
+                            <path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z"/>
+                        </svg>
+                    </a>
+                    <!-- Facebook -->
+                    <a href="https://www.facebook.com/quazi.ahmed" target="_blank" title="Facebook">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 24 24" fill="currentColor">
+                            <path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"/>
+                        </svg>
+                    </a>
+                    {{-- <!-- YouTube -->
+                    <a href="https://www.youtube.com/@QuaziAhmed" target="_blank" title="YouTube">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 24 24" fill="currentColor">
+                            <path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"/>
+                        </svg>
+                    </a>
+                    <!-- Twitter/X -->
+                    <a href="https://twitter.com/quaziahmed" target="_blank" title="Twitter">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="currentColor">
+                            <path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z"/>
+                        </svg>
+                    </a>
+                    <!-- Instagram -->
+                    <a href="https://www.instagram.com/quazi.ahmed/" target="_blank" title="Instagram">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 24 24" fill="currentColor">
+                            <path d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zm0-2.163c-3.259 0-3.667.014-4.947.072-4.358.2-6.78 2.618-6.98 6.98-.059 1.281-.073 1.689-.073 4.948 0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98 1.281.058 1.689.072 4.948.072 3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98-1.281-.059-1.69-.073-4.949-.073zm0 5.838c-3.403 0-6.162 2.759-6.162 6.162s2.759 6.163 6.162 6.163 6.162-2.759 6.162-6.163c0-3.403-2.759-6.162-6.162-6.162zm0 10.162c-2.209 0-4-1.79-4-4 0-2.209 1.791-4 4-4s4 1.791 4 4c0 2.21-1.791 4-4 4zm6.406-11.845c-.796 0-1.441.645-1.441 1.44s.645 1.44 1.441 1.44c.795 0 1.439-.645 1.439-1.44s-.644-1.44-1.439-1.44z"/>
+                        </svg>
+                    </a> --}}
+                    <!-- WhatsApp -->
+                    <a href="https://wa.me/19173653890" target="_blank" title="WhatsApp">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 24 24" fill="currentColor">
+                            <path d="M.057 24l1.687-6.163c-1.041-1.804-1.588-3.849-1.587-5.946.003-6.556 5.338-11.891 11.893-11.891 3.181.001 6.167 1.24 8.413 3.488 2.245 2.248 3.481 5.236 3.48 8.414-.003 6.557-5.338 11.892-11.893 11.892-1.99-.001-3.951-.5-5.688-1.448l-6.305 1.654zm6.597-3.807c1.676.995 3.276 1.591 5.392 1.592 5.448 0 9.886-4.434 9.889-9.885.002-5.462-4.415-9.89-9.881-9.892-5.452 0-9.887 4.434-9.889 9.884-.001 2.225.651 3.891 1.746 5.634l-.999 3.648 3.742-.981zm11.387-5.464c-.074-.124-.272-.198-.57-.347-.297-.149-1.758-.868-2.031-.967-.272-.099-.47-.149-.669.149-.198.297-.768.967-.941 1.165-.173.198-.347.223-.644.074-.297-.149-1.255-.462-2.39-1.475-.883-.788-1.48-1.761-1.653-2.059-.173-.297-.018-.458.13-.606.134-.133.297-.347.446-.521.151-.172.2-.296.3-.495.099-.198.05-.372-.025-.521-.075-.148-.669-1.611-.916-2.206-.242-.579-.487-.501-.669-.51l-.57-.01c-.198 0-.52.074-.792.372s-1.04 1.016-1.04 2.479 1.065 2.876 1.213 3.074c.149.198 2.095 3.2 5.076 4.487.709.306 1.263.489 1.694.626.712.226 1.36.194 1.872.118.571-.085 1.758-.719 2.006-1.413.248-.695.248-1.29.173-1.414z"/>
+                        </svg>
+                    </a>
+                </div>
+                <div class="footer-contact-info">
+                    <p>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="currentColor" style="vertical-align: middle; margin-right: 8px;">
+                            <path d="M0 3v18h24v-18h-24zm21.518 2l-9.518 7.713-9.518-7.713h19.036zm-19.518 14v-11.817l10 8.104 10-8.104v11.817h-20z"/>
+                        </svg>
+                        <a href="mailto:quazi@quazi.global">quazi@quazi.global</a>
+                    </p>
+                    <p>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="currentColor" style="vertical-align: middle; margin-right: 8px;">
+                            <path d="M.057 24l1.687-6.163c-1.041-1.804-1.588-3.849-1.587-5.946.003-6.556 5.338-11.891 11.893-11.891 3.181.001 6.167 1.24 8.413 3.488 2.245 2.248 3.481 5.236 3.48 8.414-.003 6.557-5.338 11.892-11.893 11.892-1.99-.001-3.951-.5-5.688-1.448l-6.305 1.654zm6.597-3.807c1.676.995 3.276 1.591 5.392 1.592 5.448 0 9.886-4.434 9.889-9.885.002-5.462-4.415-9.89-9.881-9.892-5.452 0-9.887 4.434-9.889 9.884-.001 2.225.651 3.891 1.746 5.634l-.999 3.648 3.742-.981zm11.387-5.464c-.074-.124-.272-.198-.57-.347-.297-.149-1.758-.868-2.031-.967-.272-.099-.47-.149-.669.149-.198.297-.768.967-.941 1.165-.173.198-.347.223-.644.074-.297-.149-1.255-.462-2.39-1.475-.883-.788-1.48-1.761-1.653-2.059-.173-.297-.018-.458.13-.606.134-.133.297-.347.446-.521.151-.172.2-.296.3-.495.099-.198.05-.372-.025-.521-.075-.148-.669-1.611-.916-2.206-.242-.579-.487-.501-.669-.51l-.57-.01c-.198 0-.52.074-.792.372s-1.04 1.016-1.04 2.479 1.065 2.876 1.213 3.074c.149.198 2.095 3.2 5.076 4.487.709.306 1.263.489 1.694.626.712.226 1.36.194 1.872.118.571-.085 1.758-.719 2.006-1.413.248-.695.248-1.29.173-1.414z"/>
+                        </svg>
+                        <a href="https://wa.me/8801711561861">+880 1711-561861</a>
+                    </p>
+                </div>
+            </div>
         </div>
+        
+        <div class="footer-logo">
+            <h2 class="logo-text">QUAZI M. AHMED</h2>
+            <p class="logo-tagline">Global Leadership Trainer & Executive Coach</p>
+        </div>
+        
+<div class="footer-copyright" style="text-align: center; padding: 20px 0 0;">
+    <p style="color: rgba(255, 255, 255, 0.6); font-size: 14px; margin: 0;">© {{ date('Y') }} Quazi M. Ahmed. All rights reserved.</p>
+</div>
     </div>
 </footer>
-<!-- End Footer Style Two  -->
+<!-- End Footer -->
